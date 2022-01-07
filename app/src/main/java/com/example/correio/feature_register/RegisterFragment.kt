e5801aa33984f9923ad8d696b54f88221962ab9f
Danilo Mota Filho <danilo@jeraapps.com>
2022-01-06 23:09:59 -0300

    Doing register screen

--- app/src/main/java/com/example/correio/feature_register/RegisterFragment.kt
+++ app/src/main/java/com/example/correio/feature_register/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.correio.R
 import com.example.correio.databinding.RegisterFragmentBinding
 
